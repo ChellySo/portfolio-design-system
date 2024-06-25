cf41371386855dc3b5a9a7b073cc4546211e7de7
Chelly Somopawiro <csomopawiro@inlichtingenbureau.nl>
2024-06-25 22:47:17 +0200

    hotfix: styling fixes in card and badge components

--- src/components/card/Card.stories.tsx
+++ src/components/card/Card.stories.tsx
@@ -14,10 +14,11 @@ type Story = StoryObj<typeof Card>
 
 export const cardWithChildrenAndDate: Story = {
     render: () => (
-        <Card title="Frontend Developer - Sogeti" href="./" date="01/2020 - 02-2021">
-            <Typography variant="lg/lilac">Worked on a team of 5 to develop a web application.</Typography>
-            <Typography variant="lg/lilac">Used React, TypeScript, and Tailwind CSS to build the application.</Typography>
-            <Typography variant="lg/lilac">Collaborated with the client to gather requirements and deliver a product that met their needs.</Typography>
+        <Card title="Frontend Developer - Sogeti" href="./" date="01/2020 - 02-2021" className="w-1/2">
+            <Typography variant="lg/lilac">
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            </Typography>
+
             <div className="mt-4">
                 <Badge className="mr-2">ReactJS</Badge>
                 <Badge className="mr-2">TypeScript</Badge>
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -28,7 +28,7 @@ export interface CardProps extends React.ComponentPropsWithRef<'div'> {
 
 const Card: React.FC<CardProps> = ({ className, title, href, children, date, ...rest }: CardProps) => {
     return (
-        <div className={classNames(!!date ? 'rounded-8 px-6 py-5 xs:flex' : 'rounded-8 px-6 py-5', className)} {...rest}>
+        <div className={classNames(!!date ? 'rounded-8 xs:grid xs:grid-flow-col xs:auto-cols-auto xs:gap-4' : 'rounded-8', className)} {...rest}>
             {date &&
                 <div className="xs:mr-10 my-2">
                     <Typography variant="md/lilac">{date}</Typography>
@@ -37,7 +37,7 @@ const Card: React.FC<CardProps> = ({ className, title, href, children, date, ...
 
             <div>
                 <div className="flex text-purple-200">
-                    <Heading variant="h3" className="leading-6 xs:text-heading-2">
+                    <Heading variant="h4" className="leading-6 xs:text-heading-3">
                         {title}
                     </Heading>
                     {href && <a href={href}><ArrowUpRight /></a>}
